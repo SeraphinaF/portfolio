3ff795d232329149382349887eb3922eb28b418a
seraphinaF <1028267@hr.nl>
2024-10-15 14:50:01 +0200

    only need desktop deminsions

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import './navbar.css'
-import { Link } from 'react-scroll'
-import burgerMenu from '../../assets/burgermenu.png'
+import React, { useRef, useState } from 'react';
+import './navbar.css';
+import { Link } from 'react-scroll';
+import burgerMenu from '../../assets/burgermenu.png';
+import closeIcon from '../../assets/close-icon.png';
 
 const Navbar = () => {
+  const navBarRef = useRef(null);
+  const [isNavVisible, setNavVisible] = useState(false); 
+
+  const toggleNav = () => {
+    setNavVisible(!isNavVisible); 
+  };
+
   return (
     <section className='navbar-container'>
-      <img className='burger-menu' src={burgerMenu} />
-      <nav className="navbar">
+      <img onClick={toggleNav} className='icon' id={isNavVisible ? 'close-icon' : 'burger-menu'} src={ isNavVisible ? closeIcon : burgerMenu} alt="Menu" />
+      <nav
+        className="navbar"
+        ref={navBarRef}
+        style={{ display: window.innerWidth < 1024 ? (isNavVisible ? 'block' : 'none') : 'block' }}
+        >
         <div className="menu">
           <Link activeClass="active" className="menuItem" to='home' spy={true} smooth={true} duration={500}>
-            <div class="slider"></div>Home</Link>
-          <Link activeClass="active" className="menuItem" to='portfolio' spy={true} smooth={true}  duration={500}>
-            <div class="slider"></div>Portfolio</Link>
-          <Link activeClass="active" className="menuItem" to='contact' spy={true} smooth={true} duration={500}>
-            <div class="slider"></div>Contact</Link>
-
+            <div className="slider"></div>Home
+          </Link>
+          <Link activeClass="active" className="menuItem" to='portfolio' spy={true} smooth={true} duration={500}>
+            <div className="slider"></div>Portfolio
+          </Link>
+          <Link activeClass="active" className="menuItem" to='contact' spy={true} smooth={true} spyThrottle={500} duration={500}>
+            <div className="slider"></div>Contact
+          </Link>
         </div>
       </nav>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
